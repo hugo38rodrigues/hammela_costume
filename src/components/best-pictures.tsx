4bd94deef58a8f025c4c6c01c212bb77fa95b76a
Hugo rodrigues <hugo38.rodrigues@gmail.com>
2026-02-21 17:11:21 +0100

    rename MyLogo for MyPicture

--- src/components/best-pictures.tsx
+++ src/components/best-pictures.tsx
@@ -1,15 +1,15 @@
 import { FC } from 'react'
 import { Picture } from '../lib/interface/picture'
 import { getImages } from '../lib/load-pictures'
-import { MyLogo } from './my-logo'
+import { MyPictures } from './my-logo'
 
 export const BestPictures: FC = () => {
 	const images: Picture[] = getImages()
-
+	
 	return (
 		<div className='grid grid-cols-3 gap-2 p-6 justify-items-center'>
 			{images.map((file: Picture, index) => (
-				<MyLogo
+				<MyPictures
 					alt={file.alt}
 					src={file.src}
 					key={index}

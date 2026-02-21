ce912c2a25dabf0b27a954a2457d46f473737d87
Hugo rodrigues <hugo38.rodrigues@gmail.com>
2026-02-21 17:15:35 +0100

    add filter for get last images

--- src/lib/load-pictures.ts
+++ src/lib/load-pictures.ts
@@ -4,21 +4,30 @@ import path from 'path'
 interface Picture {
 	src: string
 	alt: string
+	createdAt: Date
+	modifiedAt: Date
 }
 
 export const getImages = (): Picture[] => {
-	const uploadsDir = path.join(process.cwd(), 'public/assets')
+	const imagesDir = path.join(process.cwd(), 'public/assets')
 
-	if (!fs.existsSync(uploadsDir)) {
-		return []
+		const files = fs.readdirSync(imagesDir)
+
+		const images = files
+			.filter((file) => /\.(jpg|jpeg|png|webp|gif)$/i.test(file))
+			.map((file) => {
+				const filePath = path.join(imagesDir, file)
+				const stats = fs.statSync(filePath)
+
+				return {
+					alt: file,
+					src: `/assets/${file}`,
+					createdAt: stats.birthtime, // date de création
+					modifiedAt: stats.mtime, // date de modification
+				}
+			})
+			.sort((a, b) => b.createdAt.getMilliseconds() - a.createdAt.getMilliseconds()) // les plus récentes en premier
+			.slice(0, 6)
+		return images
 	}
 
-	const files = fs
-		.readdirSync(uploadsDir)
-		.filter((file) => /\.(jpg|jpeg|png|webp|gif)$/i.test(file))
-
-	return files.map((file) => ({
-		src: `/assets/${file}`,
-		alt: file,
-	}))
-}

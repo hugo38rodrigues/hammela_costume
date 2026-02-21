98ec13d945f4cd457097a7eaae488e3977ccd942
Hugo rodrigues <hugo38.rodrigues@gmail.com>
2026-02-21 17:10:51 +0100

    add function for send email

--- app/home/contact/page.tsx
+++ app/home/contact/page.tsx
@@ -1,13 +1,29 @@
-
+import { EmailTemplate } from '@/components/email-template'
 import { FormContact } from '@/components/form-contact'
+import { Resend } from 'resend'
+
+const resend = new Resend(process.env.RESEND_API_KEY)
 
 export default function Contact() {
-
-	const sendEmailAndDescription= async (email: string, description: string)=> {
+	const sendEmailAndDescription = async (
+		email: string,
+		description: string,
+	) => {
 		'use server'
-		console.log({ email, description })
+
+		const { error } = await resend.emails.send({
+			from: 'Contact Form <onboarding@resend.dev>',
+			to: [`${process.env.EMAIL_RECEIVER}`],
+			subject: 'Commande depuis le site',
+			react: EmailTemplate(email, description),
+		})
+
+		if (error) {
+			return { success: false }
+		}
+		return { success: true }
 	}
-	
+
 	return (
 		<>
 			<FormContact sendEmailAndDescription={sendEmailAndDescription} />

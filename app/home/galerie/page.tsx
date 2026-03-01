e67db1c7ec54461d750a002cf6ba3c609615c64e
Hugo rodrigues <hugo38.rodrigues@gmail.com>
2026-03-01 14:02:01 +0100

    add galerie

--- app/home/galerie/page.tsx
+++ app/home/galerie/page.tsx
@@ -1,3 +1,38 @@
-export default function Galerie () {
-	return (<h1>Galerie</h1>)
+import { MyPictures } from '@/components/my-logo'
+import { getPictures } from '@/lib/load-pictures'
+
+export default function Galerie() {
+	
+	const images = getPictures()
+
+	return (
+		<main
+			id='ctn-galerie'
+			className='min-w-full min-h-screen m-8 flex flex-col items-center'
+		>
+			<h1 className='text-size-title text-heading font-family-title'>
+				Bienvenue dans ma galerie
+			</h1>
+			<p>De nouveaux models sont en cours de création</p>
+			<div
+				id='ctn-pictures'
+				className='flex flex-wrap justify-center w-4xl h-9/12'
+			>
+				{images.map((images: Picture, index) => (
+					<div
+						id={`picture-${index}`}
+						className='p-4'
+						key={index}
+					>
+						<MyPictures
+							src={images.src}
+							alt={images.alt}
+							height={160}
+							width={150}
+						/>
+					</div>
+				))}
+			</div>
+		</main>
+	)
 }

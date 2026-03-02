18deaa6360aa8d0af2fefca4c7afdbb590142d1e
Hugo rodrigues <hugo38.rodrigues@gmail.com>
2026-03-02 15:03:04 +0100

    add a footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,14 +1,16 @@
-
+import Link from 'next/link'
 import { FC } from 'react'
 import { MyPictures } from './my-logo'
-import Link from 'next/link'
 
 export const Footer: FC = () => {
 	return (
-		<footer>
-			<h2>Vous pouvez me contacter sur mes autres réseaux</h2>
-			<ul>
-				<ol id='instagram'>
+		<footer className='w-full flex items-center flex-col pt-6'>
+			<h2 className='text-heading text-size-body'>Réseaux</h2>
+			<ul className='flex'>
+				<ol
+					id='instagram'
+					className='p-2'
+				>
 					<Link
 						href={`${process.env.INSTAGRAM_URL}`}
 						target='_blank'
@@ -22,7 +24,10 @@ export const Footer: FC = () => {
 						/>
 					</Link>
 				</ol>
-				<ol id='pinterest'>
+				<ol
+					id='pinterest'
+					className='p-2'
+				>
 					<Link
 						href={`${process.env.PINTEREST_URL}`}
 						target='_blank'
@@ -36,7 +41,10 @@ export const Footer: FC = () => {
 						/>
 					</Link>
 				</ol>
-				<ol id='facebook'>
+				<ol
+					id='facebook'
+					className='p-2'
+				>
 					<Link
 						href={`${process.env.FACEBOOK_URL}`}
 						target='_blank'
@@ -50,7 +58,10 @@ export const Footer: FC = () => {
 						/>
 					</Link>
 				</ol>
-				<ol id='linkedin'>
+				<ol
+					id='linkedin'
+					className='p-2'
+				>
 					<Link
 						href={`${process.env.LINKEDIN_URL}`}
 						target='_blank'
@@ -60,6 +71,23 @@ export const Footer: FC = () => {
 							alt='logo linkedin'
 							height={36}
 							src='/icons/linkedin.webp'
+							width={36}
+						/>
+					</Link>
+				</ol>
+				<ol
+					id='tiktok'
+					className='p-2'
+				>
+					<Link
+						href={`${process.env.TIKTOK_URL}`}
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						<MyPictures
+							alt='logo TikTok'
+							height={36}
+							src='/icons/tiktok.webp'
 							width={36}
 						/>
 					</Link>
